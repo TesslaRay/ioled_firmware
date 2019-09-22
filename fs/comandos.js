ac89535db9cc7b0d79df1f51db5969b08e3303f9
TesslaRay <cristian.valdivia@alumnos.usm.cl>
2019-09-22 17:27:41 -0300

    Inclusión de timer en board

--- fs/comandos.js
+++ fs/comandos.js
@@ -1,13 +1,13 @@
-mos --port /dev/cu.usbserial-14120 console
+mos --port /dev/cu.usbserial-1410 console
 
 mos --port /dev/cu.usbserial-14120 license
 
 mos --port /dev/cu.usbserial-14120 call OTA.Update '{"url":"https://github.com/TesslaRay/ioled_firmware/blob/master/build/fw.zip"}'
 
-mos --port /dev/cu.usbserial-14320 ota FIRMWARE_FILENAME_OR_URL
+mos --port /dev/cu.usbserial-1430 ota FIRMWARE_FILENAME_OR_URL
 
 // Desarrollo
-mos --port /dev/cu.usbserial-14120 gcp-iot-setup --gcp-project ioled-dev-248517 --gcp-region us-central1 --gcp-registry ioled-devices
+mos --port /dev/cu.usbserial-1410 gcp-iot-setup --gcp-project ioled-dev-248517 --gcp-region us-central1 --gcp-registry ioled-devices
 
 // Producción
 mos --port /dev/cu.usbserial-1430 gcp-iot-setup --gcp-project ioled-production --gcp-region us-central1 --gcp-registry ioled-devices
@@ -16,19 +16,23 @@ mos --port /dev/cu.usbserial-14320 ota /Users/cristian/Documents/Development/iOL
 
 mos --port /dev/cu.usbserial-14320 mdash-setup device1 506JdlOpt2TWnvAfiebUWQ
 
+
+
+
+// Wifi Oficina iOLED
 mos --port /dev/cu.usbserial-14120 wifi VTR-6439861 jkfq8yjFhwj9
 
-mos --port /dev/cu.usbserial-1430 wifi VTR-2561534 xt6JcnyhF4yf
+mos --port /dev/cu.usbserial-1410 wifi VTR-9004720 Tj3kzfbxwcrc
 
 mos --port /dev/cu.usbserial-1430 wifi POCOPHONE 23581321
 
 mos --port /dev/cu.usbserial-1430 wifi zapalloconhielo 98765432
 
-mos --port /dev/cu.usbserial-14120 flash
+mos --port /dev/cu.usbserial-1410 flash
 
 mos --port /dev/cu.usbserial-14320 build
 
-mos --port /dev/cu.usbserial-14120 put fs/mqtt.js
+mos --port /dev/cu.usbserial-1410 put fs/board.js
 
 mos --port /dev/cu.usbserial-1430 put fs/init.js
 
