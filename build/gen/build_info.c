d73f78a91ff5e7f7ba25146c498cd847f28a464a
TesslaRay <cristian.valdivia@alumnos.usm.cl>
2019-12-10 15:05:29 -0300

    i2c return

--- build/gen/build_info.c
+++ build/gen/build_info.c
@@ -1,4 +1,4 @@
 /* Auto-generated, do not edit. */
-const char *build_id = "20191125-142908";
-const char *build_timestamp = "2019-11-25T14:29:08Z";
+const char *build_id = "20191210-180344";
+const char *build_timestamp = "2019-12-10T18:03:44Z";
 const char *build_version = "2.0";
--- fs/sensors.js
+++ fs/sensors.js
@@ -3,8 +3,16 @@ load('api_i2c.js');
 // Time between each config
 let time = 2000;
 
-// let bus = I2C.get();
-// let addr = 0x64;
+let i2c = I2C.get();
+let addr = 0x40;	//64
+let dataSend = 0xE5;
+let initSensor = I2C.write(i2c, addr, dataSend, 8, stop);
+
+if (initSensor === false) {
+	print('!! - failed to init sensor - !!');
+} else {
+	print('correcto');
+}
 
 // let data = '\x12\x34';
 // let initOK = I2C.writeRegB(i2c, addr, 0x6B, 0x00); // doc for I2C.writeRegB
@@ -17,6 +25,8 @@ let publishState = function() {
 		time,
 		Timer.REPEAT,
 		function() {
+			// let temp = I2C.readRegW(i2c, addr, 0x41); //doc for I2C.readRegW 
+			// print(temp);
 			// let dataReceived = I2C.read(bus, addr, 7, true); //reading 2 bytes
 			// print(dataReceived);
 			// 	let data = I2C.read(bus, 31, 3, true);  // Read 3 bytes
