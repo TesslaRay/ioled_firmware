d22d88b7eb483e5ee08e0f070406bb69ebc4bfd5
TesslaRay <cristian.valdivia@alumnos.usm.cl>
2019-12-11 14:53:17 -0300

    Fix i2c sensor

--- fs/sensors.js
+++ fs/sensors.js
@@ -1,28 +1,31 @@
 load('api_i2c.js');
 
 // Time between each state
-let time = 60 * 1000;
+let time = 2 * 1000;
 
 let i2c = I2C.get();
-
 let addr = 64;	//0x40 - 1000000
+
+let vectorTemp = [];
 
 let publishState = function() {
 	Timer.set(
 		time,
 		Timer.REPEAT,
 		function() {
-			let Srh = I2C.readRegW(i2c, addr, 0xE5);	// 1110 0011
+			let St = I2C.readRegW(i2c, addr, 0xE3); 	// 1110 0011
+			let temp = -46.85 + 175.72* St/Math.pow(2,16);
+
+			print(vectorTemp);
+
+			let Srh = I2C.readRegW(i2c, addr, 0xE5);	// 1110 0101
 			let hum = -6 + 125* Srh/Math.pow(2,16);		
-
-			let St = I2C.readRegW(i2c, addr, 0xE3); 	// 1110 0101
-			let temp = -46.85 + 175.72* Srh/Math.pow(2,16);
-
+	
 			print('humidity: ',hum);
 			print('temperature: ',temp);
 
-			let res = MQTT.pub(stateTopic, JSON.stringify({temp: temp, hum: hum}), 1);
-			print('Published:', res ? 'yes' : 'no');
+			// let res = MQTT.pub(stateTopic, JSON.stringify({temp: temp, hum: hum}), 1);
+			// print('Published:', res ? 'yes' : 'no');
 		},
 		null
 	);
